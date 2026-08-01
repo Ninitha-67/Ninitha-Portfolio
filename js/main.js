2607686408b614e1be42cfcc890b186c526439c6
ninitha-16 <ninithanini232@gmail.com>
2026-08-01 14:31:03 +0530

    changes committedf

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ function erase() {
 }
 setTimeout(type, 1000);
 
-const roles = ['SOC Analyst', 'Threat Researcher', 'GRC Specialist', 'Penetration Tester', 'Security Engineer'];
+const roles = ['Cybersecurity Researcher', 'SOC Analyst','Junior Penetration Tester', 'Threat Intelligence Researcher', 'GRC Specialist',  'Security Engineer'];
 let roleIndex = 0;
 const roleElement = document.querySelector('.rotating-roles');
 if (roleElement) {
